f7c7326ded5dda3ced391c929602ec5d28e95eda
freddyal0x01 <freddyal0x01@gmail.com>
2023-01-31 22:08:15 -0500

    Added 2 tests to test the contract owner and withdrawal amounts

--- test/faucetTests.js
+++ test/faucetTests.js
@@ -10,16 +10,24 @@ describe("Faucet", function() {
         const Faucet = await ethers.getContractFactory("Faucet");
         const faucet = await Faucet.deploy();
 
-        const [owner] = await ethers.getSigners();
+        const [owner, signer2] = await ethers.getSigners();
+
+        let withdrawAmount = ethers.utils.parseUnits("1", "ether");
 
         console.log(`Signer 1 adddress: ${owner.address}`);
+        console.log(`Signer 2 adddress: ${signer2.address}`);
 
-        return { faucet, owner };
+        return { faucet, owner, withdrawAmount };
     }
 
     it('should deploy and set the owner correctly', async function() {
         const { faucet, owner } = await loadFixture(deployContractAndSetVariables);
 
         expect(await faucet.owner()).to.equal(owner.address);
+    });
+
+    it('should not allow withdrawals above 0.1 ETH at a time', async function() {
+        const { faucet, withdrawAmount } = await loadFixture(deployContractAndSetVariables);
+        await expect(faucet.withdraw(withdrawAmount)).to.be.reverted;
     });
 });
